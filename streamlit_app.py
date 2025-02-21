a0b571c94c55dba7bb508bbb060d318a63978bbd
严曦 <jenswe@163.com>
2025-02-21 23:45:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,10 @@ def generate_response(input_text):
 with st.form("my_form"):
     text = st.text_area(
         "Enter text:",
-        "What are the three key pieces of advice for learning how to code?",
+        "Why do I hate Elon Musk?",
     )
     submitted = st.form_submit_button("Submit")
     if not openai_api_key.startswith("sk-"):
-        st.warning("Please enter your OpenAI API key!", icon="⚠")
+        st.warning("Please enter your xAI API key!", icon="⚠")
     if submitted and openai_api_key.startswith("sk-"):
         generate_response(text)
